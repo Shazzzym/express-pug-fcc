37aaf88bedcbf2df263abf6b319c81e72b516556
Mpho Solani <solani.mpho@gmail.com>
2025-09-27 00:35:51 +0200

    Set up template engine with Pug

--- server.js
+++ server.js
@@ -6,13 +6,18 @@ const fccTesting = require('./freeCodeCamp/fcctesting.js');
 
 const app = express();
 
-fccTesting(app); //For FCC testing purposes
+// Tell Express to use Pug templates
+app.set('view engine', 'pug');
+app.set('views', './views/pug');
+
+fccTesting(app); // For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Root route → render index.pug
 app.route('/').get((req, res) => {
-
+  res.render('index', { title: 'Welcome', message: 'Hello from Pug!' });
 });
 
 const PORT = process.env.PORT || 3000;
